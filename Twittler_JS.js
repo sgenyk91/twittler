22b3e70c573d71be310cd094df1e6da6b2e51ba9
Stephan Genyk <sgenyk91@gmail.com>
2014-09-28 22:03:17 -0700

    Updated navbar

--- Twittler_JS.js
+++ Twittler_JS.js
@@ -1,34 +1,50 @@
 $(document).ready(function(){
-  $(document).on('click', '#RefreshButton', function() {
+  printTweets(currentUser);
+  $(document).on('click', '#Refresh_Button', function() {
     printTweets(currentUser);
   });
   $(document).on('click', '#tweetUser', function() {
     currentUser = $(this).text();
     printTweets(currentUser);
-  })
-  //home button
+  });
+  $(document).on('click', '#home', function() {
+    currentUser = undefined;
+    counter = 10;
+    printTweets(currentUser);
+  });
+  $(document).on('click', '#Expand_Button', function() {
+    counter +=10;
+    printTweets(currentUser);
+  });
+  /*$('#search').keypress(function(e) {
+    if(e.which == 13) {
+      currentUser = $('#search').val();
+      printTweets(currentUser);
+    }
+  });*/
 });
+
 var currentUser = undefined;
+var counter = 10;
 
 var printTweets = function(username) {
   var $tweets = $('.tweets');
   $tweets.html('');
 
   var index = streams.home.length - 1;
-
   if (!(username === undefined)) {
-    
     index = (streams.users)[username].length - 1;
   }
-  var counter = 10;
-  while(index >= 0 && counter > 0){
+
+  var newCounter = counter;
+  while(index >= 0 && newCounter > 0){
     var tweet = streams.home[index];
     if (!(username === undefined)) {
       tweet = (streams.users)[username][index];
     }
-    var tweetAt = '<span class ="tweetName"> @ </span>';
+    var tweetAt = '<span class ="tweetName" id="tweetAt"> @ </span>';
     var tweetUser = '<span class="tweetName" id="tweetUser">' + tweet.user + '</span>';
-    var tweetTime = '<span class="tweetTime"> &nbsp&nbsp&nbsp' + tweet.created_at + '</span>';
+    var tweetTime = '<span class="tweetTime" data-livestamp= "' + tweet.created_at + '"></span>';
     var tweetMessage = '<div class="tweetMessage">' + tweet.message + '</div>';
 
     $tweets.append(tweetAt);
@@ -36,6 +52,6 @@ var printTweets = function(username) {
     $tweets.append(tweetTime);
     $tweets.append(tweetMessage);
     index -= 1;
-    counter -= 1;
+    newCounter -= 1;
   }
 }
